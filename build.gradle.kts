c1f0870b6866b5c4e418c92849c123dce58660a2
ZNS <zns@example.com>
2026-08-11 17:35:57 +0600

    fix: remove unused google services and ksp plugins from root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
   alias(libs.plugins.kotlinAndroid) apply false
   alias(libs.plugins.kotlinSerialization) apply false
   alias(libs.plugins.kotlinCompose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
 }
