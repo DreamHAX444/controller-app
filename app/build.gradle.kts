4903c263901daaa458f0ba60af76707618d3b849
ZNS <zns@example.com>
2026-08-11 17:40:47 +0600

    fix: restore compose bom implementation in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ secrets {
   defaultPropertiesFileName = ".env.example"
 }
 dependencies {
+  implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
